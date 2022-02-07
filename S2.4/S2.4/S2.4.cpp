adc516f081f13b30dd58874ad9e9881f022892b6
Your Name <you@example.com>
2022-02-07 18:10:09 +0600

    new project

---
+++ S2.4/S2.4/S2.4.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(0, "");
+
+    int x = 0, r = 6350, u = 90, f = 0, d = 0;;
+
+    cout << "Введите радиус окружности ";
+    cin >> x;
+    f = r + x;
+    cout << sqrt(f * f - r * r) << endl;
+
+    system("pause");
+}
